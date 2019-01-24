eed77dd1538378af011fbfc83e092de179e04041
Amente Bekele <amenteknows@gmail.com>
2019-01-24 15:22:42 -0500

    Added correct file

--- MCPA.m
+++ MCPA.m
@@ -1,22 +1,18 @@
-function MCPA(initialX, initialV, numLoops)
+function MCPA(length, numLoops)
 %MCPA Simulates random electron scattering through a medium
 %   
-
+clc 
+close all
 %Setup initial system constraints, using user input if available
-system.force = 1E-31; %kg*m/s^2
+system.force = 0.5E-31; %kg*m/s^2
 system.deltaT = 1E-6;
 system.totalT = 0;
 electron.m = 1.109E-31; %kg
 
-if(nargin < 3)
-    electron.x = 0; %m
-    electron.v = 0; %m/s
-    numLoops = 100;
-else
-    electron.x = initialX; 
-    electron.v = initialV;
-    
-end
+electron.x = zeros(1,length); %m
+electron.v = zeros(1,length); %m/s
+electron.driftv = zeros(1,length);
+
 
 ForceMratio = system.force./electron.m;
 
@@ -24,28 +20,48 @@ ForceMratio = system.force./electron.m;
 figure(1)
 xGraph = subplot(2,1,1);
 plot(xGraph, electron.x, system.totalT);
-xGraph = title('Position vs. Time of Randomly Scattering Electron', 'Interpreter', 'Latex');
-xGraph = xlabel('Time (s)', 'Interpreter', 'Latex');
-xGraph = ylabel('Position (m)', 'Interpreter', 'Latex');
+xGraphTitle = title('Position vs. Time of Randomly Scattering Electron', 'Interpreter', 'Latex');
+xGraphXlabel = xlabel('Time (s)', 'Interpreter', 'Latex');
+xGraphYlabel = ylabel('Position (m)', 'Interpreter', 'Latex');
 hold on
 vGraph = subplot(2,1,2);
-vGraph = plot(electron.v, system.totalT);
-vGraph = title('Velocity vs. Time of Randomly Scattering Electron', 'Interpreter', 'Latex');
-vGraph = xlabel('Time (s)', 'Interpreter', 'Latex');
-vGraph = ylabel('Velocity ($\frac{m}{s}$)', 'Interpreter', 'Latex');
+plot(electron.v, system.totalT);
+vGraphTitle = title('Velocity vs. Time of Randomly Scattering Electron, v = ', 'Interpreter', 'Latex');
+vGraphXlabel = xlabel('Time (s)', 'Interpreter', 'Latex');
+vGraphYlabel = ylabel('Velocity ($\frac{m}{s}$)', 'Interpreter', 'Latex');
 hold on
 
 
 %Calculate new velocity and position From F = ma
 for i = 1:numLoops
-    system.totalT = system.totalT + system.deltaT;
-    
-    electron.x = electron.x + ForceMratio.*system.deltaT.^2;
-    electron.v = electron.v + ForceMratio.*system.deltaT;
-    
-    plot(xGraph, electron.x, system.totalT);
-    plot(vGraph, electron.v, system.totalT);
-   
+    for j = 1:length
+        system.totalT = system.totalT + system.deltaT;
+
+        electron.x(j) = electron.x(j) + electron.v(j).*system.deltaT;
+        electron.v(j) = electron.v(j) + ForceMratio.*system.deltaT;
+
+        if(j == 1)
+            plot(xGraph, system.totalT,electron.x(1), 'r.');
+            hold on
+            plot(vGraph, system.totalT,electron.v(1), 'b.');
+            hold on
+            title(['Velocity vs. Time of Randomly Scattering Electron, v =' num2str(mean(electron.x(1)/system.totalT))], 'Interpreter', 'Latex');
+        end
+
+        if (rand()>0.95)
+            electron.v(j)=0;
+        end
+    end
+    if mod(numLoops, 100) == 0
+        fprintf('%i \n', i);
+    end
+    pause(0.01);
+end
+for i=1:length
+   electron.driftv(i) = electron.x(i)/system.totalT; 
 end
 
+title(['Velocity vs. Time of Randomly Scattering Electron, v =' num2str(mean(electron.driftv))], 'Interpreter', 'Latex');
+
+fprintf('Done \n');
 
